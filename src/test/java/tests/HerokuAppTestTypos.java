5977102d4c05767f774235d9634474b7d8c650e1
Vitaliy <40178281+vitalik33811@users.noreply.github.com>
2021-10-31 19:37:54 +0300

    textFromElement

--- src/test/java/tests/HerokuAppTestTypos.java
+++ src/test/java/tests/HerokuAppTestTypos.java
@@ -21,10 +21,10 @@ public class HerokuAppTestTypos {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://the-internet.herokuapp.com/typos");
-        String addElement = driver.findElement(By.cssSelector("div > p:nth-of-type(1)")).getText();
-        Assert.assertEquals(addElement, "This example demonstrates a typo being introduced. It does it randomly on each page load.");
-        String addElement1 = driver.findElement(By.cssSelector("div > p:nth-of-type(2)")).getText();
-        Assert.assertEquals(addElement1, "Sometimes you'll see a typo, other times you won't.");
+        String textFromElement = driver.findElement(By.cssSelector("div > p:nth-of-type(1)")).getText();
+        Assert.assertEquals(textFromElement, "This example demonstrates a typo being introduced. It does it randomly on each page load.");
+        String textFromElement1 = driver.findElement(By.cssSelector("div > p:nth-of-type(2)")).getText();
+        Assert.assertEquals(textFromElement1, "Sometimes you'll see a typo, other times you won't.");
         driver.quit();
     }
 }
